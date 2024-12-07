2ad875743c3914b5dd2b3cc89b91a7d7a6ad7a4a
Anton A Shanaurin <stud0000298878@utmn.ru>
2024-12-07 21:23:29 +0500

    homework

---
+++ eslint.config.mjs
@@ -0,0 +1,11 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  pluginJs.configs.recommended,
+  {
+    languageOptions: { globals: globals.browser },
+    files: ["src/**/*.js"],
+  }
+];
